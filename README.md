cd09e484dc3d60d720e19ccdf193df97054541f9
Wen Zhou <517565244@qq.com>
2016-10-15 21:08:50 +0800

    glob support

--- README.md
+++ README.md
@@ -2,5 +2,31 @@
 	```url 
 	https://gist.github.com/josephspurrier/90e957f1277964f26852
 	```
+
 2. 文件列表保存在的output.log文件中
+
+3. 文件列表中打印目录和文件的信息，目录没有计算md5值
+
+4. 使用方法：
+	
+	* 列出当前目录下的所有文件
+	```shell
+	go run tree.go ./ 
+	```
+
+	* 列出当前目录下的所有文件，并过滤output.log文件
+	```shell
+	go run tree.go ./ output.log
+	```
+
+	* 列出当前目录下的所有文件，并过滤所有的txt文件
+	```shell 
+	go run tree.go ./  *.txt
+	```
+
+	* 列出当前目录下的所有文件，并过滤.git目录和output.log文件
+	```shell 
+	go run tree.go ./ output.log .git 
+	```
+
 
--- tree.go
+++ tree.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"crypto/md5"
+	"errors"
 	"fmt"
+	"io"
 	"math"
 	"os"
-	"errors"
 	"path/filepath"
-	"crypto/md5"
-	"io"
 )
 
 const CHUNK = 8192
 
 func writeToFile(f *os.File, filename string, md5Value string) {
-	_, err := f.WriteString(fmt.Sprintf("%s    %s\n", filename, md5Value))
+	// Get file info
+	info, err := os.Stat(filename)
+	if err != nil {
+		err = errors.New(fmt.Sprintf("Cannot access file:", filename))
+		return
+	}
+
+	// Get the file size
+	fileSize := info.Size()
+
+	_, err = f.WriteString(fmt.Sprintf("%s,  %s, %d\n", filename, md5Value, fileSize))
 	if err != nil {
 		fmt.Printf("Write file fail: %v", err)
 	}
@@ -66,16 +76,20 @@ func getFileMd5(filename string) (md5Value string, err error) {
 }
 
 func usage() {
-	fmt.Println("go run tree.go path")
+	fmt.Println("Usage: go run tree.go <path> [ignore_patterns]")
 }
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) < 2 {
 		usage()
 		return
 	}
 
 	directory := os.Args[1]
+	ignorePatterns := make([]string, 0)
+	for i := 2; i < len(os.Args); i++ {
+		ignorePatterns = append(ignorePatterns, os.Args[i])
+	}
 
 	fileInfo, err := os.Stat(directory)
 	if err != nil {
@@ -96,20 +110,45 @@ func main() {
 	}
 	defer outputFile.Close()
 
+	
 	// Traversal the directory
 	err = filepath.Walk(directory, func(path string, f os.FileInfo, err error) error {
 		if f == nil {
 			return nil
 		}
-		if f.IsDir() {
-			return nil
+
+		matched := false
+		for _, pattern := range ignorePatterns {
+			m, err := filepath.Match(path, pattern)
+			fmt.Printf("path is %s, pattern is %s, m is %v\n", 
+			path, pattern, m)
+			if err != nil {
+				fmt.Printf("filepath.Match returned %v\n", err)
+			} else {
+				if m == true {
+					matched = true
+				}
+			}
 		}
 
-		md5Value, err := getFileMd5(path)
-		if err != nil {
-			fmt.Printf("getFileMd5 returned %v\n", err)
+		// ignore
+		if matched == true {
+			if f.IsDir() {
+				return filepath.SkipDir
+			} else {
+				return nil
+			} 
+		}
+		
+		if f.IsDir() {
+			writeToFile(outputFile, path, "")
 		} else {
-			writeToFile(outputFile, path, md5Value)
+			md5Value, err := getFileMd5(path)
+			if err != nil {
+				fmt.Printf("getFileMd5 returned %v\n", err)
+			} else {
+				writeToFile(outputFile, path, md5Value)
+			}
 		}
 		return nil
 	})
